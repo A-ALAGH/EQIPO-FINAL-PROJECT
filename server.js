507e3c7ac8cc52dab8607aa3c5133f685b0eabe3
A-ALAGH <drlaghoub@gmail.com>
2023-06-28 14:12:41 +0100

    cors configured

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const userRoutes = require ('./routes/userRoutes')
 const sportEventRoutes = require ('./routes/sportEventRoutes')
 const loisirEventRoutes = require ('./routes/loisirEventRoutes')
 const demandeRoutes = require ('./routes/demandeRoutes')
+const cors = require('cors');
+
 require('dotenv').config();
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
 
 app.use('/api/user', userRoutes)
 app.use('/api/sportevent', sportEventRoutes)
